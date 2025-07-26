04d1be5305412b8c37416623921134f8f58b94e5
Jaisingh <jaising062757@gmail.com>
2025-07-26 21:25:07 +0530

    allUniquePath recursive solution in js

---
+++ Dynamic-Programming/allUniquePath-in-js.js
@@ -0,0 +1,16 @@
+
+function allUniquePath(i,j){
+    if(i==0 && j==0)
+        return  1
+    if(i<0 || j < 0)
+        return 0
+    else
+        return allUniquePath(i-1, j) + allUniquePath(i,j-1)
+}
+
+
+
+const m = 3, n = 2;
+console.log(allUniquePath(m-1,n-1))
+
+
