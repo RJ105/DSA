092ebd2ebc7304239c607ad6bbf0981662454b07
Jaisingh <jaising062757@gmail.com>
2026-03-02 22:47:43 +0530

    fibonacci time and space complexity

--- Dynamic-Programming/fibonacci.py
+++ Dynamic-Programming/fibonacci.py
@@ -1,9 +1,15 @@
+#recursive solution 
 def fibo(n):
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
+    if(n<=1):
+        return n
     return fibo(n-1) + fibo(n-2)
 
 n = 10
 print(fibo(n))
+
+# Time complexity : O(2^n)
+# space complexity : O(n)
+
+# -----------------------------------------------------------------------------
+    # Memoization
+
