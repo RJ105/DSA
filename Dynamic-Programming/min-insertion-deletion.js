dffda1b6cebe1363cebf92ddcc0e97b9336738bf
Jaisingh <jaising062757@gmail.com>
2026-04-08 22:20:42 +0530

    solved min-insertion-deletion using memo

--- Dynamic-Programming/min-insertion-deletion.js
+++ Dynamic-Programming/min-insertion-deletion.js
@@ -1,5 +1,6 @@
 //Given two strings s1 and s2, find the minimum number of insertions and deletions required to convert s1 into s2.
 
+//-----------------------------Recursive implementation--------------
 function minOperationRecursive(s, t, m, n){
         if(m<0) return n+1 // source is over, we have to insert remaining chars of target in source to acheive target
 
@@ -15,6 +16,25 @@ function minOperationRecursive(s, t, m, n){
     }
 }
 
+//-----------------------------Memoization implementation--------------
+function minOperationMemo(s, t, m, n, memo){
+    if(m<0) return n+1 // source is over, we have to insert remaining chars of target in source to acheive target
+
+    if(n<0) return m+1 // target length is over, we have to delete remaining chars of source to acheive target
+
+    if(memo[m][n] != -1)
+        return memo[m][n]
+    if(s[m] === t[n]){
+        memo[m][n] = minOperationMemo(s, t, m-1, n-1, memo)
+        return memo[m][n]
+    }
+    else{
+        let insertion = 1 + minOperationMemo(s, t, m, n-1, memo)
+        let deletion = 1 + minOperationMemo(s, t, m-1, n, memo)
+        memo[m][n] = Math.min(insertion, deletion)
+        return memo[m][n] 
+    }
+}
 
 const testCases = [
   { s1: "heap",    s2: "pea",     expDel: 2, expIns: 1, expTotal: 3 },
@@ -33,10 +53,10 @@ console.log("=".repeat(60));
 testCases.forEach(({ s1, s2, expDel,  expIns, expTotal}, i) => {
     let m = s1.length
     let n = s2.length
-    const result   = minOperationRecursive(s1, s2, m-1, n-1);
+    // const result   = minOperationRecursive(s1, s2, m-1, n-1);
 
-    // memo = Array.from({length : m+1}, ()=> new Array(n+1).fill(-1))
-    // const result   = lcsMemo(s1, s2, m-1, n-1, memo);
+    memo = Array.from({length : m+1}, ()=> new Array(n+1).fill(-1))
+    const result   = minOperationMemo(s1, s2, m-1, n-1, memo);
     // const result  = lcsTabulation(s1, s2, m, n);
 
   const pass = result === expTotal
