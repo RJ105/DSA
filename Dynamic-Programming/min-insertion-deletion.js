60fe2a4227187208a4ca9de03605d12bbe9fc056
Jaisingh <jaising062757@gmail.com>
2026-04-09 22:51:05 +0530

    solved min-insertion-deletion using tabulation

--- Dynamic-Programming/min-insertion-deletion.js
+++ Dynamic-Programming/min-insertion-deletion.js
@@ -36,6 +36,28 @@ function minOperationMemo(s, t, m, n, memo){
     }
 }
 
+
+//-----------------------------Tabulation implementation--------------
+
+function lcsTabulation(s1, s2, m, n){
+    const dp = Array.from({length: m+1}, ()=> new Array(n+1).fill(0))
+
+    for(let i=0; i<=m; i++)
+        dp[i][0] = i
+    for(let j=0; j<=n; j++)
+        dp[0][j] = j
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (s1[i - 1] === s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = 1+ Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+   }
+   return dp[m][n]
+}
+
 const testCases = [
   { s1: "heap",    s2: "pea",     expDel: 2, expIns: 1, expTotal: 3 },
   { s1: "AGGTAB",  s2: "GXTXAYB", expDel: 2, expIns: 3, expTotal: 5 },
@@ -55,9 +77,9 @@ testCases.forEach(({ s1, s2, expDel,  expIns, expTotal}, i) => {
     let n = s2.length
     // const result   = minOperationRecursive(s1, s2, m-1, n-1);
 
-    memo = Array.from({length : m+1}, ()=> new Array(n+1).fill(-1))
-    const result   = minOperationMemo(s1, s2, m-1, n-1, memo);
-    // const result  = lcsTabulation(s1, s2, m, n);
+    // memo = Array.from({length : m+1}, ()=> new Array(n+1).fill(-1))
+    // const result   = minOperationMemo(s1, s2, m-1, n-1, memo);
+    const result  = lcsTabulation(s1, s2, m, n);
 
   const pass = result === expTotal
 
